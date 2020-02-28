7ff2a02aea7817cbb465cc1e84c513ab608fb6f0
aweikn <w_k_guo@126.com>
2020-02-29 01:13:59 +0800

    添加后台server

---
+++ server/src/main/java/com/aweikn/springseed/SpringseedApplication.java
@@ -0,0 +1,13 @@
+package com.aweikn.springseed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringseedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringseedApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/aweikn/springseed/SpringseedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aweikn.springseed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringseedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
